f45b0ab1e581a21a3d9300bb515965f8e66862e9
SHAMIL K <64640025+shamilkotta@users.noreply.github.com>
2023-01-02 16:26:41 +0530

    test: bad pr

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
-import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import './index.css';
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
