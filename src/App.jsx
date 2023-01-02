000cc6e6ca77dd7e1a5a271368d610a640a95d59
shamilkotta <shamilkotta99@gmail.com>
2023-01-02 20:10:57 +0530

    feat: todo layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,101 @@
-import React from "react";
-import logo from "./logo.svg";
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/prop-types */
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todoData, setTodoData] = useState([]);
+  const [openForm, setOpenForm] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="todo-task todo-container">
+        <h3>Todo</h3>
+        <div className="todo-data">
+          {todoData.map((data, index) => (
+            <div className="todo-card" key={index}>
+              <h4>{data.title}</h4>
+              <p>{data.description}</p>
+            </div>
+          ))}
+        </div>
+        <button
+          type="button"
+          className="todo-container-add"
+          onClick={() => {
+            setOpenForm(true);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          + Add item
+        </button>
+        {openForm && (
+          <AddTodoItemForm
+            onClose={() => {
+              setOpenForm(false);
+            }}
+            onSubmit={(newdata) => {
+              setTodoData((data) => [...data, newdata]);
+            }}
+          />
+        )}
+      </div>
+      <div className="todo-progress todo-container">
+        <div className="todo-container-head">
+          <h3>Todo</h3>
+          <div>+</div>
+        </div>
+      </div>
+      <div className="todo-done todo-container">
+        <div className="todo-container-head">
+          <h3>Todo</h3>
+          <div>+</div>
+        </div>
+      </div>
     </div>
+  );
+}
+
+function AddTodoItemForm({ onClose, onSubmit }) {
+  const [formData, setFormData] = useState({ title: "", description: "" });
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    onSubmit(formData);
+    setFormData({ title: "", description: "" });
+  };
+
+  return (
+    <form
+      className="form-card"
+      onSubmit={(e) => {
+        submitForm(e);
+      }}
+    >
+      <button type="button" className="form-close" onClick={onClose}>
+        X
+      </button>
+      <input
+        type="text"
+        name="title"
+        id="title"
+        placeholder="Title"
+        value={formData.title}
+        onChange={(e) => {
+          setFormData((data) => ({ ...data, title: e.target.value }));
+        }}
+      />
+      <input
+        type="text"
+        name="description"
+        id="description"
+        placeholder="Description"
+        value={formData.description}
+        onChange={(e) => {
+          setFormData((data) => ({ ...data, description: e.target.value }));
+        }}
+      />
+      <button type="submit">Add</button>
+    </form>
   );
 }
 
